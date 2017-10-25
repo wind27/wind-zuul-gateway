df51a42feaf562355e76dbd593d12d46d1aa10fe
qianchun <qianchun@huli.com>
2017-10-25 14:53:38 +0800

    init

--- src/main/java/com/wind/zuul/WindZuulGatewayApplication.java
+++ src/main/java/com/wind/zuul/WindZuulGatewayApplication.java
@@ -4,9 +4,17 @@ import com.netflix.zuul.ZuulFilter;
 import com.wind.zuul.filter.GatewayFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.*;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
+
+@Import({ DispatcherServletAutoConfiguration.class, EmbeddedServletContainerAutoConfiguration.class,
+		ErrorMvcAutoConfiguration.class, HttpEncodingAutoConfiguration.class,
+		HttpMessageConvertersAutoConfiguration.class, JacksonAutoConfiguration.class, MultipartAutoConfiguration.class,
+		ServerPropertiesAutoConfiguration.class, WebMvcAutoConfiguration.class })
 @EnableZuulProxy
 @SpringBootApplication
 public class WindZuulGatewayApplication {
